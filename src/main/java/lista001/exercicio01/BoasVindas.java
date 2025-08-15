4ac46319e780d329caf632dfe5bf825c0f523ad4
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-08-14 21:36:13 -0300

    :sparkles: feat(lista001): realizar primeira atividade

---
+++ src/main/java/lista001/exercicio01/BoasVindas.java
@@ -0,0 +1,17 @@
+/*
+    1. Ambiente de Desenvolvimento
+    Exercício:
+    Você está começando a usar Java e quer testar se o ambiente está configurado corretamente.
+    No JShell, imprima a mensagem de boas-vindas ao sistema que você está criando, e depois crie um programa BoasVindas que imprime:
+    Bem-vindo ao Sistema de Geração de Jogadores!
+    Saída esperada:
+
+    Bem-vindo ao Sistema de Geração de Jogadores!
+*/
+package lista001.exercicio01;
+public class BoasVindas{
+    public static void main(String[] args){
+        System.out.println("Bem-vindo ao Sistema de Geração de Jogadores!");
+    }
+}
+
