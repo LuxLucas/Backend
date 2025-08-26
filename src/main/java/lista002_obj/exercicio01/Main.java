6f1aecab70f25b1b49d58353b0be1ef39aeb31cd
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-08-26 19:10:49 -0300

    :sparkles: feat(lista002_obj): permitir ao usuário criar uma nova senha por meio do console

--- src/main/java/lista002_obj/exercicio01/Main.java
+++ src/main/java/lista002_obj/exercicio01/Main.java
@@ -1,7 +1,14 @@
 package lista002_obj.exercicio01;
 
+import java.util.Scanner;
+
 public class Main{
     public static void main (String[] args){
-
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Tamanho da senha: ");
+        int passwordLength = scan.nextInt();
+        Hasher hasher = new Hasher(passwordLength);
+        System.out.println("Sua senha é: " + hasher.newPassword()); 
+        scan.close();
     }
 }
